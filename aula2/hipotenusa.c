a72adb0be9517309940d42b3b867d3ec88c721cf
Pedro Canto <pmcanto@gmail.com>
2024-03-04 14:51:55 -0300

    Create hipotenusa.c

---
+++ aula2/hipotenusa.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+    float cat1, cat2;
+    printf("Digite o valor do 1 cateto: \n" );
+    scanf("%f",&cat1);
+
+    printf("Digite o valor do 2 cateto: \n" );
+    scanf("%f",&cat2);
+	
+	float hip;
+	
+	hip = sqrt(pow(cat1,2) + pow(cat2,2))
+	
+    printf("A hipotenusa de %.2f e %.2f é %.2f.", cat1, cat2, hip);
+    return 0;
+}
